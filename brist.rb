8746a822d28cf8d91f26e7c95f1565f7d5ed84b7
Wojciech Wnętrzak <w.wnetrzak@gmail.com>
2013-07-15 19:14:18 +0200

    more content

--- brist.rb
+++ brist.rb
@@ -1,16 +1,30 @@
 require "sinatra"
-
-set :reload_templates, true
+require "bridge"
 
 helpers do
-  def inline(name)
-    content = erb(name)
+  def inline(name, options)
+    content = erb(name, options)
     content.gsub("\n", '\n').gsub('"', '\"')
+  end
+
+  def bids_offset(dealer)
+    case dealer
+    when "W" then []
+    when "N" then [""]
+    when "E" then ["", ""]
+    when "S" then ["", "", ""]
+    end
   end
 end
 
-get "/brist.js" do
-  erb :index
+get "/brist/:id.js" do
+  deal = Bridge::Deal.from_id(params[:id].to_i)
+  erb :index, locals: {
+    deal: deal,
+    dealer: params[:d].upcase,
+    vulnerable: params[:v].upcase,
+    auction: Bridge::Auction.new(params[:d].upcase, params[:a].split(",").map(&:upcase))
+  }
 end
 
 get "/brist.css" do
---
+++ config.ru
@@ -0,0 +1,2 @@
+require "./brist"
+run Sinatra::Application
